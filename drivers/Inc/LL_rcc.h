9739a5841eb9d9811432b9c17523a4e30df34af3
vikrant thakur <vikrantthakur97@gmail.com>
2020-08-18 13:41:58 +0530

    peripheral-clock-register

--- drivers/Inc/LL_rcc.h
+++ drivers/Inc/LL_rcc.h
@@ -11,8 +11,23 @@
 
 typedef struct
 {
-	vo uint32_t  CR;
+	vo uint32_t  CR; 												/*!< configure the mode address offset - 0x00>*/
 	vo uint32_t PLL_CFGR;
+	vo uint32_t  CFGR;
+	vo uint32_t CIR;
+	vo uint32_t AHB1RSTR;
+	vo uint32_t AHB2RSTR;
+	vo uint32_t AHB3RSTR;
+	vo uint32_t APB1RSTR;
+	vo uint32_t	APB2RSTR;
+	uint32_t       RESERVED[2];
+	vo uint32_t	AHB1ENR;
+	vo uint32_t	AHB2ENR;
+	vo uint32_t	AHB3ENR;
+	uint32_t       RESERVED[1];
+	vo uint32_t	APB1ENR;
+	vo uint32_t  APB2ENR;
+	uint32_t       RESERVED[2];
 };
 
 #endif /* INC_LL_RCC_H_ */
